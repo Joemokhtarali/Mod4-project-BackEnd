45e0875ddc80d363e5d7a16aee6f33724f2a1163
Jay Wen <jaywen89@gmail.com>
2020-03-27 13:44:46 -0400

    updated tree image

--- db/migrate/20200320145248_create_trees.rb
+++ db/migrate/20200320145248_create_trees.rb
@@ -1,7 +1,7 @@
 class CreateTrees < ActiveRecord::Migration[6.0]
   def change
     create_table :trees do |t|
-      t.string :image, default: 'https://toppng.com/uploads/preview/ine-trees-silhouette-clipart-panda-pine-tree-vector-115630115124z9ezkopbw.png' 
+      t.string :image, default: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F09%2F29%2F02%2F45%2Ftree-1702020_960_720.png' 
       t.string :size, default: 'small'
       t.integer :oxygen, default: 3 
       t.integer :carbon_dioxide, default: -2
@@ -12,3 +12,7 @@ class CreateTrees < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+
+# new png image for tree
+#  https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F09%2F29%2F02%2F45%2Ftree-1702020_960_720.png
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_145248) do
   end
 
   create_table "trees", force: :cascade do |t|
-    t.string "image", default: "https://toppng.com/uploads/preview/ine-trees-silhouette-clipart-panda-pine-tree-vector-115630115124z9ezkopbw.png"
+    t.string "image", default: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2016%2F09%2F29%2F02%2F45%2Ftree-1702020_960_720.png"
     t.string "size", default: "small"
     t.integer "oxygen", default: 3
     t.integer "carbon_dioxide", default: -2
